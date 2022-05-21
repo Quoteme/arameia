56a8545d00cee0cbd977ded451c58d6acd1e5177
Luca Leon Happel <luca.happel@uni-duesseldorf.de>
2022-05-21 13:36:59 +0200

    added entity parsing and improved memory allocations

--- src/entity.c
+++ src/entity.c
@@ -1,7 +1,10 @@
 #include "entity.h"
+#include "direction.h"
+#include "point.h"
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_image.h>
 #include <SDL2/SDL_rect.h>
+#include <SDL2/SDL_render.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,41 +16,162 @@
  * @param position position of the entity
  * @return pointer to the memory which stores the entity
  */
-Entity *newEntity(char name[10], Point2D_d position, Point2D_d size){
+Entity *newEntity(char *name) {
   Entity *e = NULL;
   e = malloc(sizeof(Entity));
   if (!e) {
     fprintf(stderr, "Memory allocation failed");
+    fprintf(stderr, "Could not allocate memory for entity %s", name);
     exit(1);
   };
+  e->name = malloc(strlen(name));
+  if (!e->name) {
+    fprintf(stderr, "Memory allocation failed!\n");
+    fprintf(stderr, "Could not allocate memory for entity name %s!\n", name);
+  }
   strcpy(e->name, name);
-  e->position = position;
-  e->size = size;
   return e;
 }
+
+/**
+ * @brief Clear the memory allocated for an entity
+ *
+ * @param e The entity which should be removed
+ */
+void removeEntity(Entity *e) {
+  free(e->name);
+  free(e);
+}
+
+/**
+ * @brief Parse health given a line. This is used in parseEntity, to
+ * parse an entities health. A line has the pattern:
+ * ```
+ * 100 98
+ * ```
+ * This would yield: (Health) {.full = 100, .now = 98};
+ *
+ * @param line Line containing health information
+ * @return Health that was parsed from the input line
+ */
+Health parseHealth(char *line){
+  Point2D_i p = parsePoint2D_i(line);
+  Health h = (Health) {
+    .full = p.x,
+    .now = p.y
+  };
+  return h;
+}
+
+/**
+ * @brief Given a line which stores an entity (like in a level file,
+ * loaded by `loadLevel`), allocate memory for the described
+ * entity and fill this memory with the fitting values.
+ * Example line:
+ * ## entity_name
+ * POS: posX posY
+ * DIM: sizeX sizeY
+ * VEL: velocityX velocityY
+ * DIR: direction2D
+ * HEL: healthFull healthNow
+ * ANI: animationSheetURL
+ * ~
+ *
+ * @param fp File pointer to a file where an entity needs to be parsed.
+ * Which entity gets parsed is determined by the stream position, whic
+ * can be read by `fgetpos`.
+ * @return The first entity that can be read in the file pointed to
+ * by the parameter `fp` after the current stream position.
+ * If no entity can be read, a NULL pointer will be returned.
+ */
+Entity *parseEntity(FILE *fp) {
+  Entity *e;
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  char firstThreeChars[3];
+  read = getline(&line, &len, fp);
+  memcmp(firstThreeChars, line, 3);
+  if (line[0]==']' || !strcmp(firstThreeChars, "## ")){
+    return e;
+  }else{
+    e = newEntity(line + 3);
+    e->name[strlen(e->name)-1] = '\0';
+  }
+  while ((read = getline(&line, &len, fp)) != -1) {
+    if (line[0] == '~')
+      break;
+    memcpy(firstThreeChars, line, 3);
+    line[strlen(line)-1] = '\0';
+    if (strcmp(firstThreeChars, "POS")==0) {
+      e->position = parsePoint2D_d(line+5);
+    } else if (strcmp(firstThreeChars, "DIM")==0) {
+      e->size = parsePoint2D_d(line+5);
+    } else if (strcmp(firstThreeChars, "VEL")==0) {
+      e->velocity = parsePoint2D_d(line+5);
+    } else if (strcmp(firstThreeChars, "DIR")==0) {
+      e->direction = parseDirection2D(line+5);
+    } else if (strcmp(firstThreeChars, "HEL")==0) {
+      e->health = parseHealth(line+5);
+    } else if (strcmp(firstThreeChars, "ANI")==0) {
+      e->animationSheet = loadAnimationSheet(line+5);
+    }
+  }
+  return e;
+};
+
+/**
+ * @brief AnimationSheets are not automatically included, when a newEntity
+ * is called. This needs to be executed after calling `newEntity`
+ * for the entity to have an AnimationSheet
+ *
+ * @param as AnimationSheet
+ * @param e Entity
+ */
+void addAnimationSheetToEntity(AnimationSheet *as, Entity *e) {
+  e->animationSheet = as;
+};
 
 /**
  * @brief Print information about an entity to stdout
  *
  * @param entity Entity to print information about
  */
-void printEntity(const Entity *entity){
+void printEntity(const Entity *entity) {
   if (entity == NULL) {
     printf("NULL entity\n");
   } else {
-    printf("Entity \"%s\" at %lf %lf\n", entity->name, entity->position.x, entity->position.y);
+    printf("Entity \"%s\" at %lf %lf\n", entity->name, entity->position.x,
+           entity->position.y);
   }
 }
 
-void drawEntity(SDL_Renderer * renderer, Entity * entity) {
+void drawEntity(SDL_Renderer *renderer, Entity *entity) {
+  // TODO: make this draw an image
   SDL_Rect rect = {
-    .x = entity->position.x,
-    .y = entity->position.y,
-    .w = entity->size.x,
-    .h = entity->size.y,
+      .x = entity->position.x,
+      .y = entity->position.y,
+      .w = entity->size.x,
+      .h = entity->size.y,
   };
   SDL_SetRenderDrawColor(renderer, 255, 100, 100, 100);
   SDL_RenderFillRect(renderer, &rect);
+}
+
+/**
+ * @brief Create a new entitylist
+ *
+ * @return Pointer to an entitylist
+ */
+EntityList *newEntityList() {
+  EntityList *el = malloc(sizeof(EntityList));
+
+  if (!el) {
+    fprintf(stderr, "Memory allocation failed!\n");
+    fprintf(stderr, "Could not allocate a new EntityList!\n");
+    exit(1);
+  }
+  return el;
 }
 
 /**
@@ -56,18 +180,16 @@ void drawEntity(SDL_Renderer * renderer, Entity * entity) {
  * @param entityList EntityList to which an entity should be appended
  * @param e Entity to append
  */
-void addEntity(EntityList * entityList, Entity * e) {
-  if (entityList->entity==NULL) {
-    printf("Es existiert bisher noch kein Element in dieser Liste\n");
+void addEntity(EntityList *entityList, Entity *e) {
+  if (!entityList->entity) {
     entityList->entity = e;
   } else {
-    printf("! Es existiert bereits ein Element in dieser Liste\n");
-    entityList->next = malloc(sizeof(EntityList));
-    if (!entityList->next) {
-      fprintf(stderr, "Memory allocation failed!");
-      exit(1);
+    EntityList *el = entityList;
+    while (el->next) {
+      el = el->next;
     }
-    entityList->next->entity = e;
+    el->next = newEntityList();
+    el->next->entity = e;
   }
 }
 
@@ -76,8 +198,8 @@ void addEntity(EntityList * entityList, Entity * e) {
  *
  * @param entityList The EntityList which is supposed to be cleared
  */
-void clearEntityList(EntityList *entityList){
-  forEachEntity(entityList, free);
+void clearEntityList(EntityList *entityList) {
+  forEachEntity(entityList, removeEntity);
 }
 
 /**
@@ -87,7 +209,7 @@ void clearEntityList(EntityList *entityList){
  * @param f The function which should be executed with an entity as
  * an argument for each entity
  */
-void forEachEntity(EntityList * entityList, void (*f)()){
+void forEachEntity(EntityList *entityList, void (*f)()) {
   EntityList *el = entityList;
   f(el->entity);
   if (el->next) {
@@ -102,10 +224,10 @@ void forEachEntity(EntityList * entityList, void (*f)()){
  * @param entityList The entitylist for which the length should be calculated
  * @return The length of the entitylist
  */
-int amountOfEntities(EntityList * entityList){
+int amountOfEntities(EntityList *entityList) {
   printf("test\n");
-  int i=0;
-  EntityList* el = entityList;
+  int i = 0;
+  EntityList *el = entityList;
   while (el) {
     i++;
     el = el->next;
--- src/entity.h
+++ src/entity.h
@@ -7,6 +7,7 @@
 #ifndef ENTITY
 #define ENTITY
 
+#include "animation.h"
 #include "point.h"
 #include "direction.h"
 #include <SDL2/SDL.h>
@@ -15,17 +16,22 @@ typedef struct {
   int full, now;
 } Health;
 
+Health parseHealth(char *line);
+
 typedef struct {
   Point2D_d position;
   Point2D_d size;
   Point2D_d velocity;
   Direction2D direction;
   Health health;
-  char name[10];
+  char *name;
+  AnimationSheet *animationSheet;
 } Entity;
 
-Entity *newEntity(char[10], Point2D_d, Point2D_d);
+Entity *newEntity(char*);
 void removeEntity(Entity*);
+Entity *parseEntity(FILE*);
+void addAnimationSheetToEntity(AnimationSheet*, Entity*);
 
 void printEntity(const Entity*);
 
@@ -37,6 +43,7 @@ struct EntityList {
   EntityList* next;
 };
 
+EntityList *newEntityList();
 void addEntity(EntityList*, Entity*);
 /* EntityList mapEntity(EntityList* , Entity (*f)); */
 void forEachEntity(EntityList* , void (*f)());
